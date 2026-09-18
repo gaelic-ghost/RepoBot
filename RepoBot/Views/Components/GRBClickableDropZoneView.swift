421dbe283bdd44a6255bcc9936a6cc8a50e3d556
Gale W <mail@galewilliams.com>
2026-09-18 12:46:13 -0400

    misc: Skeleton: More thoughts and initial planning

---
+++ RepoBot/Views/Components/GRBClickableDropZoneView.swift
@@ -0,0 +1,26 @@
+//
+//  GRBClickableDropZoneView.swift
+//  RepoBot
+//
+//  Created by Gale Williams on 9/18/26.
+//
+
+import SwiftUI
+
+struct GRBClickableDropZoneView: View {
+
+		// drop zone accepting specified types, onClick opens the nspanel for specified types
+		// need to store bookmark for access to the dropped/added content, and register an fs watcher for moves/changes
+		// Might put this in GRBDetailView or trailing inspector... or maybe in the toolbar add button, with this expanding onHover of the toolbar button..? idk yet...
+
+	// TODO: - Add an array of acceptable filetypes, passed in on .init
+	// TODO: - Add actions for adding a file for a ProjectType, adding a dir of files for a ProjectType, adding a new enabled ProjectType, etc...
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GRBClickableDropZoneView()
+}
--- RepoBot/Views/GRBRootView.swift
+++ RepoBot/Views/GRBRootView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct GRBRootView: View {
     var body: some View {
 
+		// TODO: - Add toolbar
+
 		NavigationSplitView {
 			GRBSidebarView()
 		} content: {
 			GRBContentView()
+			// Might consider a trailing inspector, instead.
 		} detail: {
 			GRBDetailView()
 		}
