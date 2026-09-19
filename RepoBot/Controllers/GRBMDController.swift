aa31e3432182a49bbf916e4fbe1981127dbfb1a7
Gale W <mail@galewilliams.com>
2026-09-18 20:00:39 -0400

    misc: Maint: add gitignore and md controller

---
+++ RepoBot/Controllers/GRBMDController.swift
@@ -0,0 +1,11 @@
+//
+//  GRBMDController.swift
+//  RepoBot
+//
+//  Created by Gale Williams on 9/18/26.
+//
+
+import Foundation
+
+// Engine for handling templatd markdown docs like README.md and ROADMAP.md and CONTRIBUTING.md and whatnot
+// Prolly use swift-markdown, it's a nice walker-based lib... or, maybe parse from md to AttributedString, prolly simpler that way, and build a nice GUI editor view for repo docs templating, including CRUD for sections, and also text moves? idk...
--- RepoBot/GRBApp.swift
+++ RepoBot/GRBApp.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 @main struct GRBApp: App {
+
+
     var body: some Scene {
 
 			// TODO: - Add Menu Bar Scene after designing rest of UI
@@ -11,6 +13,11 @@ import SwiftUI
         WindowGroup {
 			GRBRootView()
         }
+
+		// Grab LocalizedStringResource as pref, maybe? I forget, double check this later... Might just want a WindowGroup for this, tbh...
+		Window(<#T##titleResource: LocalizedStringResource##LocalizedStringResource#>, id: "mdTemplating") {
+			// GRBMDTemplatingView here..?
+		}
 		Settings() {
 			GRBSettingsView()
 		}
